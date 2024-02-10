4c5620c0b530d3913b652dcf5ad44179564f8408
abdo elsharkawy <abdoelsharkawy16>
2024-02-10 21:29:00 +0200

    fyh

--- lib/features/auth/view/screens/sign_up.dart
+++ lib/features/auth/view/screens/sign_up.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-class home extends StatelessWidget {
-  const home({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
