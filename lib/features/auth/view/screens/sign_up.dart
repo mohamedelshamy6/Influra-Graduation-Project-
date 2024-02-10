9f2e07f786f600ea29a2efced8854513f8bcbdbc
abdo elsharkawy <abdoelsharkawy16>
2024-02-10 21:22:07 +0200

    f

---
+++ lib/features/auth/view/screens/sign_up.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
